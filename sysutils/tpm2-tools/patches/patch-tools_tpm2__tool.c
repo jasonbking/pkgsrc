715865e006da13d52977575113d9dbac923dc84b
Jason King <jason.brian.king@gmail.com>
2025-05-13 02:26:11 +0000

    Update tpm2-tools to 5.7

--- sysutils/tpm2-tools/patches/patch-tools_tpm2__tool.c
+++ sysutils/tpm2-tools/patches/patch-tools_tpm2__tool.c
@@ -2,13 +2,13 @@ $NetBSD$
 
 Add proper include for rindex(3C)
 
---- tools/tpm2_tool.c.orig	2024-03-18 05:32:40.674130667 +0000
+--- tools/tpm2_tool.c.orig	2024-04-26 11:29:46.000000000 +0000
 +++ tools/tpm2_tool.c
 @@ -3,6 +3,7 @@
  #include <stdbool.h>
  #include <stdlib.h>
  #include <string.h>
 +#include <strings.h>
+ #include <libgen.h>
  
  #include <openssl/err.h>
- #include <openssl/evp.h>
