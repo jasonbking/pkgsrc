466f6eb097e56e06e8074ecea62e656b83f187bf
Jason King <jason.brian.king@gmail.com>
2025-05-13 01:54:12 +0000

    Update tpm2-tss package to 4.1.3

--- sysutils/tpm2-tss/patches/patch-src_tss2-tcti_tcti-device.c
+++ sysutils/tpm2-tss/patches/patch-src_tss2-tcti_tcti-device.c
@@ -1,16 +1,13 @@
 $NetBSD$
 
-Limit tpmrm0 device to Linux
-
---- src/tss2-tcti/tcti-device.c.orig	2024-03-30 06:07:04.107066435 +0000
+--- src/tss2-tcti/tcti-device.c.orig	2024-05-21 12:44:23.000000000 +0000
 +++ src/tss2-tcti/tcti-device.c
-@@ -61,7 +61,9 @@ static char *default_conf[] = {
- #ifdef __VXWORKS__
+@@ -58,7 +58,7 @@
+ #include "util/log.h"
+ 
+ static char *default_conf[] = {
+-#ifdef __VXWORKS__
++#if defined(__VXWORKS__) || defined(__illumos__)
      "/tpm0"
  #else
-+#ifdef __linux__
      "/dev/tpmrm0",
-+#endif
-     "/dev/tpm0",
- #endif /* __VX_WORKS__ */
- };
